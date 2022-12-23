d34948702de951edf1c64da53956b1c292892a47
Nutan <nutanrani10704@gmail.com>
2022-12-23 10:27:54 +0000

    added functionality to add and delete  to do to array

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const todos = [];
-
-
+let todos = [];
 
 const addTodo = (todoName) => {
-
+  console.log(todoName);
+  todos.push(todoName);
 };
 
-const deleteTodo = (todoName) => {
-
+const deleteTodo = (todoIndex) => {
+    console.log(typeof todoIndex);
+    const tempTodos = [];
+    for (let i=0;i<todos.length;i++){
+       if(i!==todoIndex){
+         tempTodos.push(todos[i]);  
+       } 
+    }
+    todos = tempTodos;
+   
+    
 };
-
-
-
